9d46be3332d2d643d779248f9d77f2fa00e394cc
siaka-s <siakasiahoue1@gmail.com>
2025-11-12 17:11:44 +0000

    saisi de la taches et du statut de priorite

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-type Priority = "urgent" | "moyenne" | "Basse"
+import { useState } from "react"
+
+type Priority = "Urgente" | "Moyenne" | "Basse"
 
 type Todo = {
   id:number,
@@ -8,17 +10,34 @@ type Todo = {
 
 function App() {
 
+  const [input,setInput] = useState("")
+  const [priority,setPriority] = useState<Priority>("Moyenne")
+
   return (
 
-    <div className="flex justify-center">
+    <div className="flex justify-center rounded-2xl">
 
-      <div className="w-2/3 flex flex-col gap-4 bg-base-300 my-15 p-5 rounded-2xl">
-        <div className="flex p-3 gap-4">
+      <div className="w-2/3 flex flex-col gap-4 my-15 p-5 rounded-2xl">
+        <div className="flex p-10 gap-4 bg-base-300">
           <input  
             type="text"
             className="input w-full"
-            placeholder="saisisez la tâche"
+            placeholder="saisisez votre la tâche..."
+            value={input}
+            onChange={(e) => {setInput(e.target.value)}}
           />
+          <select
+            className="select w-ful rounded-4xl justify-center"
+            value={priority}
+            onChange={(e) => {setPriority(e.target.value as Priority)}}
+            >
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Basse</option>
+          </select>
+          <button className="btn btn-primary">
+            Ajouter
+          </button>
         </div>
       </div>
        
